a275963f6d3b5a640a5ac551c629212b87bff390
Sokomoto <wdws2342319@gmail.com>
2020-12-19 17:10:06 +0900

    java_Chap04_再学習1

---
+++ JavaWcp/src/Chapter04_2.java
@@ -0,0 +1,12 @@
+
+public class Chapter04_2 {
+	public static void main(String[] args) {
+		int luck_num = 7;
+		String name = "Kido Futa";
+		System.out.println(name);
+		name = "Java勉強中";
+		System.out.println(name);
+		System.out.println(luck_num);
+	}
+
+}
