cfb0b158a471c0c015e607049e424d75e8a21127
Sokomoto <wdws2342319@gmail.com>
2020-12-20 18:07:48 +0900

    chap5_2

---
+++ JavaWcp/src/Chapter05_02.java
@@ -0,0 +1,14 @@
+
+public class Chapter05_02 {
+	public static void main(String[] args) {
+		long l = 100000000L;
+		double d = 3.14;
+		boolean b = true;
+		System.out.println(l);
+		System.out.println(d);
+		System.out.println(b);
+
+
+	}
+
+}
