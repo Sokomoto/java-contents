c7e9de1709301f0c7b3a69462060fdbf36a93a59
Sokomoto <wdws2342319@gmail.com>
2021-01-05 17:01:34 +0900

    java_pra

---
+++ JavaWcp/src/Chapter05_2.java
@@ -0,0 +1,20 @@
+
+public class Chapter05_2 {
+	public static void main(String[]args) {
+		long l = 1000000000L;
+		double d = 1.1456;
+		boolean b = true;
+		Float f = 10.111111f;
+		Integer i = 10;
+		Character c = 'あ';
+		int[] in = { 10, 20, 30, 40, 50};
+		System.out.println(l);
+		System.out.println(d);
+		System.out.println(b);
+		System.out.println(f);
+		System.out.println(i);
+		System.out.println(c);
+		System.out.println(in[3]);
+	}
+
+}
