4ddfa535da645e7fcbfec68230a9736a8b2e72f9
Sokomoto <wdws2342319@gmail.com>
2021-01-07 00:53:19 +0900

    java_pra

---
+++ JavaWcp/src/kidofuta/java_practice9.java
@@ -0,0 +1,11 @@
+package kidofuta;
+
+public class java_practice9 {
+	public static void main(String[]args) {
+		String s = "SPAM";
+		for (int i = 0; i < 10; i++) {
+			System.out.println(s);
+		}
+	}
+
+}
