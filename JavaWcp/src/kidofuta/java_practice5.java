bc177fe46db5de656f9cefa2779caa18a21f08f5
Sokomoto <wdws2342319@gmail.com>
2021-01-06 16:54:34 +0900

    java_pra

---
+++ JavaWcp/src/kidofuta/java_practice5.java
@@ -0,0 +1,15 @@
+package kidofuta;
+
+public class java_practice5 {
+	public static void main(String[]args) {
+		int num = 6;
+		if (num % 2 == 0) {
+			// if (num % 2 != 0) 奇数判別
+			System.out.println("偶数です");
+		}
+		else {
+			System.out.println("奇数です");
+		}
+	}
+
+}
