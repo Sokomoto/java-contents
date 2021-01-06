9ddb7fc1e365f63f102891a0c18c9ff3b9580a0d
Sokomoto <wdws2342319@gmail.com>
2021-01-06 16:36:06 +0900

    java_pra

---
+++ JavaWcp/src/kidofuta/java_practice2.java
@@ -0,0 +1,13 @@
+package kidofuta;
+
+public class java_practice2 {
+	public static void main(String[]args) {
+		int x = 5;
+		int y = 10;
+		if (x < y) {
+			System.out.println(y);
+			//これでいいのか不明
+		}
+	}
+
+}
