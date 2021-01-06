d3810c37c3cf5f67b8fef9d8fa82a8270322b9d0
Sokomoto <wdws2342319@gmail.com>
2021-01-06 17:23:33 +0900

    java_pra

---
+++ JavaWcp/src/kidofuta/java_practice6.java
@@ -0,0 +1,16 @@
+package kidofuta;
+
+public class java_practice6 {
+	public static void main(String[]args) {
+		int num = 3;
+		if (num % 2 == 0) {
+			System.out.println("正の偶数");
+		}
+		else if (num % 2 != 0) {
+			System.out.println("正の奇数");
+		}
+		//負の条件分岐の方法がわからない
+
+	}
+
+}
