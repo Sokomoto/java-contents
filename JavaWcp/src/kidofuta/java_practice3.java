36a92c3bf365be183c254820bcf905f7bb7f2287
Sokomoto <wdws2342319@gmail.com>
2021-01-06 16:47:08 +0900

    java_pra

---
+++ JavaWcp/src/kidofuta/java_practice3.java
@@ -0,0 +1,16 @@
+package kidofuta;
+
+public class java_practice3 {
+	public static void main(String[]args) {
+		int x = 15;
+		int y = 10;
+
+		if (y < x) {
+			System.out.println("xはyより大きい");
+		}
+		else {
+			System.out.println("xはyより小さい");
+		}
+	}
+
+}
