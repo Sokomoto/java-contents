5268552f84bda2531731ef1aec29e28087386a28
Sokomoto <wdws2342319@gmail.com>
2021-01-06 14:57:19 +0900

    java_pra

--- JavaWcp/src/java_practice010.java
+++ JavaWcp/src/java_practice010.java
@@ -2,15 +2,26 @@
 public class java_practice010 {
 	public static void main(String[]args) {
 		// あってるか不明 要確認
+		//int x = 2;
+		//x *= x;
+		//System.out.println(x);
+
+		//x *= x;
+		//System.out.println(x);
+
+		//x *= x;
+		//System.out.println(x);
+
+
+		// 以下、解答例
 		int x = 2;
-		x *= x;
+		// xの一乗
 		System.out.println(x);
+		// xの二乗
+		System.out.println(x * x);
+		// xの三乗
+		System.out.println(x * x * x);
 
-		x *= x;
-		System.out.println(x);
-
-		x *= x;
-		System.out.println(x);
 	}
 
 }
