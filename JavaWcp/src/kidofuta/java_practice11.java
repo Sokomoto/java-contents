c0cc3572ff9adf6c1c12491553cc54496ef79c3b
Sokomoto <wdws2342319@gmail.com>
2021-01-08 08:31:02 +0900

    java_pra

---
+++ JavaWcp/src/kidofuta/java_practice11.java
@@ -0,0 +1,12 @@
+package kidofuta;
+
+public class java_practice11 {
+	public static void main(String[]args) {
+		int num = 12;
+		while (num > 0) {
+			num -= 2;
+			System.out.println(num);
+		}
+	}
+
+}
