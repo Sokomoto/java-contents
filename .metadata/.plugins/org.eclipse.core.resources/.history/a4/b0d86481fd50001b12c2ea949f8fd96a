6eb5d683e88d566438d387d0f7bf58b9a0857d7a
Sokomoto <wdws2342319@gmail.com>
2021-01-08 00:32:39 +0900

    java_pra

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/a4/b0d86481fd50001b12c2ea949f8fd96a
@@ -0,0 +1,12 @@
+package kidofuta;
+
+public class java_practice10 {
+	public static void main(String[]args) {
+		int num = 1;
+		while (num < 5) {
+			System.out.println(num * num);
+			num++;
+		}
+	}
+
+}
---
+++ JavaWcp/src/kidofuta/java_practice07.java
@@ -0,0 +1,17 @@
+package kidofuta;
+
+public class java_practice07 {
+	public static void main(String[]args) {
+		//int num = 1;
+		//while (num <= 10) {
+		//	System.out.print(num + (num < 10 ? "," : ""));
+		//	num++;
+		//}
+
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.print(i + (i < 10 ? "," : ""));
+		}
+	}
+
+}
