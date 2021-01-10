b2996328bb523d0821acf9b60722ea2e62f5b40d
Sokomoto <wdws2342319@gmail.com>
2021-01-10 09:46:38 +0900

    java_pra

---
+++ JavaWcp/src/kidofuta/java_practice20.java
@@ -0,0 +1,17 @@
+package kidofuta;
+
+public class java_practice20 {
+	public static void main(String[]args) {
+		int even = 0;
+		int evensum = 0;
+		for (int i = 1; i <= 10; i++) {
+			if (i % 2 == 0) {
+				even++;
+				evensum += i;
+			}
+		}
+		System.out.println("偶数の個数は、" + even + "です。");
+		System.out.println("偶数の合計は、" + evensum + "です。");
+	}
+
+}
