8fa17d89b1bb8d26b49c3a515923a13ad8e5139e
Sokomoto <wdws2342319@gmail.com>
2021-01-12 17:34:44 +0900

    java_pra

---
+++ JavaWcp/src/kidofuta/java_practice21.java
@@ -0,0 +1,12 @@
+package kidofuta;
+
+public class java_practice21 {
+	public static void main(String[]args) {
+		for (int i = 10; i >= 0; i--) {
+			if (i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
