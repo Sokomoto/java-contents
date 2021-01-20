452bf01d2a100e1462c12de0e0fff257f9804251
Sokomoto <wdws2342319@gmail.com>
2021-01-20 11:15:37 +0900

    java

---
+++ JavaWcp/src/kidofuta/java_practice26.java
@@ -0,0 +1,13 @@
+package kidofuta;
+
+public class java_practice26 {
+	public static void main(String[]args) {
+		for (int i = 1; i <= 10; i++) {
+			System.out.print(i);
+				if (i < 10) {
+					System.out.print(",");
+				}
+		}
+	}
+
+}
